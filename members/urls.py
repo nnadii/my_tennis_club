fdf85fbdf1adcdf362631988d80ca29749c65091
Chimennadi <nnadichime04@gmail.com>
2024-06-05 09:54:53 +0100

    making template files

--- members/urls.py
+++ members/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.members, name="members")
+    path("", views.members, name="members"),
+    path("details/<int:id>", views.details, name="details")
 ]
--- members/views.py
+++ members/views.py
@@ -10,3 +10,10 @@ def members(request):
     template = loader.get_template("all_members.html")
     context = {"mymembers": mymembers}
     return HttpResponse(template.render(context, request))
+
+
+def details(request, id):
+    mymember = Member.objects.get(id=id)
+    template = loader.get_template("details.html")
+    context = {"mymember": mymember}
+    return HttpResponse(template.render(context, request))
